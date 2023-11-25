aeb99600015072ac5322756bf8c945223f3696fa
John <jbigelow2@ivytech.edu>
2023-11-25 16:09:53 -0500

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include #I personally added this because include wasn't a recognized keyword
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
